3a4f07cf846cc1c4dafb1524c5b665087872d9b7
Abigail <nahualhuelabigail03@gmail.com>
2025-09-17 11:27:02 -0300

    agrego App.tsx

--- frontend_proyecto/src/App.tsx
+++ frontend_proyecto/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import DetalleDepartamento from "./components/departamento/departamentos";
+import type { Departamento, Carrera } from "./types/types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [departamento, setDepartamento] = useState<Departamento | null>(null);
+  const [carreras, setCarreras] = useState<Carrera[]>([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/departamentos/1")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Departamento:", data);
+        setDepartamento(data);
+      })
+      .catch((err) => console.error("Error cargando departamento:", err));
+
+    fetch("http://127.0.0.1:8000/carreras/departamento/1")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Carreras:", data);
+        setCarreras(Array.isArray(data) ? data : []);
+      })
+      .catch((err) => console.error("Error cargando carreras:", err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {departamento ? (
+        <DetalleDepartamento departamento={departamento} carreras={carreras} />
+      ) : (
+        <p>Cargando departamento...</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
