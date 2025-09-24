d9a52846dc8a797aa3dd41c9f0857bb012e9c474
Abigail <nahualhuelabigail03@gmail.com>
2025-09-24 20:14:15 -0300

    actualizo frontend con cambios completos de dev

--- frontend_proyecto/src/services/informesService.ts
+++
@@ -1,9 +0,0 @@
-export async function fetchInformes() {
-  const res = await fetch("http://localhost:8000/informes/");
-  return res.json();
-}
-
-export async function fetchInforme(id: string) {
-  const res = await fetch(`http://localhost:8000/informes/${id}`);
-  return res.json();
-}
