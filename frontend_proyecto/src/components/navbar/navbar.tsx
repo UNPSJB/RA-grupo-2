e614db06bd23e537ec971f494f94a9cd00188686
Abigail <nahualhuelabigail03@gmail.com>
2025-10-06 15:57:21 -0300

    actualizo frontend con los últimos cambios de kiara

--- frontend_proyecto/src/components/navbar/navbar.tsx
+++ frontend_proyecto/src/components/navbar/navbar.tsx
@@ -30,6 +30,7 @@ export default function Navbar() {
             <Link className="nav-link" to="/departamento">Carreras</Link>
             <Link className="nav-link" to="/informes">Informes Sintéticos</Link>
             <Link className="nav-link" to="/preguntas/crear">Crear Pregunta</Link>
+            <Link className="nav-link" to="/encuestas/categoria-b">Completar Encuesta</Link>
           </div>
         </div>
       </div>
