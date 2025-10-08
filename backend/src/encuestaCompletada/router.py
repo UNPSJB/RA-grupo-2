6d57fab80ec597c1bd7164ddf68f96da3ad7aeb4
Abigail <nahualhuelabigail03@gmail.com>
2025-10-08 18:31:26 -0300

    validar encuestas respondidas por el alumno

--- backend/src/encuestaCompletada/router.py
+++ backend/src/encuestaCompletada/router.py
@@ -3,8 +3,15 @@ from sqlalchemy.orm import Session
 from src.database import get_db
 from src.encuestaCompletada import schemas, services
 from typing import List
+from src.asociaciones.models import Periodo
 
 router = APIRouter(prefix="/encuesta-completada", tags=["encuestas-completadas"])
+
+@router.get("/existe")
+def verificar_encuesta_completada(alumno_id: int, encuesta_id: int, materia_id: int, anio: int, periodo: Periodo, db: Session = Depends(get_db)):
+    existe = services.verificar_encuesta_existente(db, alumno_id, encuesta_id, materia_id, anio, periodo)
+    return {"existe": existe}
+
 
 @router.post("/", response_model=schemas.EncuestaCompletada)
 def crear_encuesta_completada(encuesta: schemas.EncuestaCompletadaCreate, db: Session = Depends(get_db)):
@@ -16,7 +23,6 @@ def crear_encuesta_completa_con_respuestas(encuesta_data: schemas.EncuestaComple
     return services.crear_encuesta_completa_con_respuestas(db, encuesta_data)
 
 
-
 @router.get("/{encuesta_id}", response_model=schemas.EncuestaCompletada)
 def obtener_encuesta_completada(encuesta_id: int, db: Session = Depends(get_db)):
     return services.obtener_encuesta_completada(db, encuesta_id)
@@ -24,4 +30,5 @@ def obtener_encuesta_completada(encuesta_id: int, db: Session = Depends(get_db))
 @router.get("/alumno/{alumno_id}", response_model=List[schemas.EncuestaCompletada])
 def obtener_encuestas_por_alumno(alumno_id: int, db: Session = Depends(get_db)):
     return services.obtener_encuestas_por_alumno(db, alumno_id)
+
 
