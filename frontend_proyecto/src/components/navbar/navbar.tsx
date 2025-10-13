4a8c3d2a9dc5d033221b6fc0590fb3fdf5ac993e
Kiara <kiaralk79@gmail.com>
2025-10-13 00:21:08 -0300

    dejando navbar prolijo

--- frontend_proyecto/src/components/navbar/navbar.tsx
+++ frontend_proyecto/src/components/navbar/navbar.tsx
@@ -1,38 +1,126 @@
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      {/* 
-        fixed-top: Navbar FIJO en la parte superior (clase de Bootstrap)
-        bg-primary: Color azul de Bootstrap (puedes cambiar por otro)
-      */}
       <div className="container-fluid">
         <Link className="navbar-brand fw-bold" to="/">
           Sistema de encuestas
         </Link>
-        
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
           data-bs-target="#navbarContent"
-          aria-controls="navbarContent" 
-          aria-expanded="false" 
+          aria-controls="navbarContent"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        
+
         <div className="collapse navbar-collapse" id="navbarContent">
-          <div className="navbar-nav">
-            <Link className="nav-link" to="/encuestas">Encuestas</Link>
-            <Link className="nav-link" to="/docentes/1">Docentes</Link>
-            <Link className="nav-link" to="/departamento">Carreras</Link>
-            <Link className="nav-link" to="/informes">Informes Sintéticos</Link>
-            <Link className="nav-link" to="/preguntas/crear">Crear Pregunta</Link>
-            <Link className="nav-link" to="/encuestas-completadas">Encuestas Completadas</Link>
-            <Link className="nav-link" to="/informes-catedra">Informes Catedra</Link>
-          </div>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="alumnoDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Alumno
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="alumnoDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/encuestas">
+                    Encuestas Disponibles
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/encuestas-completadas">
+                    Encuestas Completadas
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="docenteDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Docente
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="docenteDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/docentes/1">
+                    Materias dadas
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="departamentoDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Departamento
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="departamentoDropdown"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/departamento">
+                    Carreras
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/informes-catedra">
+                    Informes Cátedra
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="secretariaDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Secretaría Académica
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="secretariaDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/informes">
+                    Informes Sintéticos
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/preguntas/crear">
+                    Crear Pregunta
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
