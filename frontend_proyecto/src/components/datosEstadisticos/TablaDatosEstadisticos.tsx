76457866ca1931ea0d77cb88d6c1364dc9efcb80
Abigail <nahualhuelabigail03@gmail.com>
2025-10-30 15:06:40 -0300

    implemento componente Acordeon en tabla de datos estadísticos

--- frontend_proyecto/src/components/datosEstadisticos/TablaDatosEstadisticos.tsx
+++ frontend_proyecto/src/components/datosEstadisticos/TablaDatosEstadisticos.tsx
@@ -1,3 +1,5 @@
+import Acordeon from "../acordeon/Acordeon";
+
 interface OpcionPorcentaje {
   opcion_id: string;
   porcentaje: number;
@@ -22,38 +24,39 @@ export default function TablaDatosEstadisticos({ datos , cant }: TablaProps) {
     );
   }
 
-  return (
-    <div className="card mt-4">
-      <div className="card-header bg-primary text-white">
-        <h2 className="h5 mb-0">Datos Estadísticos - {cant} encuestas completadas</h2>
-      </div>
-
-      <div className="card-body">
-        {datos.map((pregunta, i) => (
-          <div key={i} className="mb-4">
-            <h5 className="text-primary">
-              {pregunta.id_pregunta}
-            </h5>
-            
-            <table className="table table-striped table-bordered mt-2">
-              <thead className="table-light">
-                <tr>
-                  <th>Opción</th>
-                  <th>Porcentaje</th>
+  const contenido = (
+    <div className="card-body">
+      {datos.map((pregunta, i) => (
+        <div key={i} className="mb-4">
+          <h5 className="text-dark">
+            {pregunta.id_pregunta}
+          </h5>
+          
+          <table className="table table-striped table-bordered mt-2">
+            <thead className="table-light">
+              <tr>
+                <th>Opción</th>
+                <th>Porcentaje</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pregunta.datos.map((opcion, j) => (
+                <tr key={j}>
+                  <td>{opcion.opcion_id}</td>
+                  <td>{opcion.porcentaje}%</td>
                 </tr>
-              </thead>
-              <tbody>
-                {pregunta.datos.map((opcion, j) => (
-                  <tr key={j}>
-                    <td>{opcion.opcion_id}</td>
-                    <td>{opcion.porcentaje}%</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ))}
-      </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
+  );
+
+  return(
+    <Acordeon
+      titulo={`Datos Estadísticos - ${cant} encuestas completadas`}
+      contenido={contenido}
+    />
   );
 }
