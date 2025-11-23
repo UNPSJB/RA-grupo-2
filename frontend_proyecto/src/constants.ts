1ddd6a37966b720dc1895f30ffa30c03d04d39ab
Kiara <kiaralk79@gmail.com>
2025-11-23 03:23:54 -0300

    alumno

--- frontend_proyecto/src/constants.ts
+++ frontend_proyecto/src/constants.ts
@@ -2,7 +2,7 @@ export const ANIO_ACTUAL = 2025;
 export const PERIODO_ACTUAL = "SEGUNDO_CUATRI";
 export const DOCENTE_ID = 1; // ID del docente logueado
 export const CARRERA_ID = 1; // ID fija para informes sintéticos base
-export const ALUMNO_ID = 12;
+export const ALUMNO_ID = 8;
 
 export function MostrarPeriodo(p: string) {
   switch (p) {
